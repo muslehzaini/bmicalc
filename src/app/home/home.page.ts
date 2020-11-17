4c544d481b775ef1a768da3eb792bd612d2c77d1
Musleh_Zaini <muslehzaini@gmail.com>
2020-11-17 10:28:19 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  //declare global variable
+  //
+  height
+  weight
+  bmi
+  message
 
   constructor() {}
+
+
+ buttonPressed(){
+ 	//local, cannot call into home
+
+ 	let heightCM = this.height/100
+
+	this.bmi  = (this.weight/ Math.pow((heightCM),2)).toFixed(2)
+
+	if (this.bmi <= 18){
+		this.message="Underweight"
+	}
+	else if(this.bmi >= 18 && this.bmi <=25){
+		this.message="Normal"
+	}
+	else if(this.bmi >= 25 && this.bmi <=30){
+		this.message="Overweight"
+	} 
+	else{
+		this.message="Obese"
+	}
+	
+
+
+	
+
+
+	//console.log(this.height)
+	//console.log(this.weight)
+ }
 
 }
